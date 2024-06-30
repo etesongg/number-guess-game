da313cdac4d7ebe3db3143f8953cdb146c2ef23e
etesong014 <etesong0114@gmail.com>
2024-06-30 19:20:03 +0900

    js 2기 1-2 과정

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # number-guess-game 만들기(javascript-study 1-7)
 
 ## Up-Down GAME
-![numberguessgame](https://github.com/etesongg/number-guess-game/assets/55964387/7879f0db-7a12-42e5-b463-046561d1f4ca)
-<br/>
-[위 게임 접속하기](https://number-guess-gamebysone.netlify.app/)
 
 ## 기능
 1. 컴퓨터는 게임이 시작할때 랜덤한 숫자를 뽑는다
--- main.js
+++ main.js
@@ -10,101 +10,26 @@
 // 유저가 이미 입력한 숫자를 또 입력하면 알려주고 기회를 깍지 않음
 
 let computerNum = 0;
-let chances;
-let gameOver = false;
-let userInputList;
-
 let playButton = document.getElementById("play-button");
-let resetButton = document.getElementById("reset-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
-let chanceInput = document.getElementById("chance-range");
-let chanceArea = document.getElementById("chance-area");
-let rangeInput = document.getElementById("extent-range");
-let rangeArea = document.getElementsByClassName("form-label")[0];
 
-playButton.addEventListener("click", play);
-resetButton.addEventListener("click", reset);
-userInput.addEventListener("focus", function() {
-    userInput.value = ""
-})
-
-rangeArea.textContent = `업다운 범위 : ${rangeInput.value}`;
-rangeInput.addEventListener("input", (event) => {
-    rangeArea.textContent = `업다운 범위 : ${event.target.value}`;
-    pickRandomNum()
-});
-
-chanceArea.textContent = `남은 횟수 : ${chanceInput.value}`;
-chanceInput.addEventListener("input", (event) => {
-    chanceArea.textContent = `남은 횟수 : ${event.target.value}`;
-    chances = event.target.value;
-})
+playButton.addEventListener("click", play); // 함수를 매개변수로 넘김 play() 괄호 넣으면 안됨
 
 function pickRandomNum() {
-    computerNum = Math.floor(Math.random()*rangeInput.value) + 1; // Math.random() 메서드는 0-1 사이에 숫자를 제공 그러므로 *rangeInput.value과 소수이하 버리기, +1 을 통해 1-rangeInput.value까지의 숫자로 만들어 줘야 함 
+    computerNum = Math.floor(Math.random() * 100) + 1; // Math.random() 0~1 숫자를 반환(이때 1은 포함 안되는 1에 가까운 숫자를 반환)
+    console.log(computerNum);
 }
-pickRandomNum()
-
-// 남은 횟수, 업다운 범위의 max 값 수정 후 변경해야 하는 값들
-let initialChanceValue = 15;
-let initialRangeValue = 160;
-
-function initializeGame() {
-    userInputList = []
-    chances = chanceInput.value; 
-    gameOver = false;
-
-    chanceInput.value = initialChanceValue;
-    rangeInput.value = initialRangeValue;
-
-    chanceArea.textContent = "남은 횟수: " + chanceInput.value;
-    rangeArea.textContent = "업다운 범위: " + rangeInput.value;
-}
-initializeGame()
 
 function play() {
     let userValue = userInput.value;
-
-    // 유효성 검사
-    let maxExtentValue = parseInt(rangeInput.max);
-    if (userValue > maxExtentValue || userValue < 1){
-        resultArea.textContent = `1 ~ ${rangeInput.value} 사이의 숫자를 입력해 주세요.`;
-        return; // 밑에 코드를 실행시키지 않는다.
-    }
-
-    if (userInputList.includes(userValue)){
-         resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요.";
-         return;
-    }
-
-    chances--;
-    chanceArea.textContent = `남은 횟수 : ${chances}`;
-
     if (userValue < computerNum){
-        resultArea.textContent = "UP!!!"
+        resultArea.textContent = "up"
     }else if (userValue > computerNum){
-        resultArea.textContent = "Down!!!"
+        resultArea.textContent = "down"
     }else {
-        resultArea.textContent = "정답입니다!!!"
-        gameOver = true
-    }
-
-    userInputList.push(userValue)
-
-    if (chances < 1){
-        gameOver = true
-    }
-
-    if (gameOver == true){
-        playButton.disabled = true;
+        resultArea.textContent = "정답"
     }
 }
 
-function reset() {
-    userInput.value = ""
-    pickRandomNum()
-    resultArea.textContent = "결과창"
-    playButton.disabled = false;
-    initializeGame()
-}
+pickRandomNum();
